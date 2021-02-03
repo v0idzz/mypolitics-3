28627a888d89c0cf57102b8d4a318efb63407b05
Antoni Kepinski <xxczaki@pm.me>
2021-02-03 12:46:49 +0100

    add icons & urls, UX improvements

--- src/shared/ShareSocial/ShareSocialView.tsx
+++ src/shared/ShareSocial/ShareSocialView.tsx
@@ -1,47 +1,83 @@
 import React from "react";
 import Button from "@shared/Button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTwitter,
+  faFacebookF,
+  faDiscord,
+} from "@fortawesome/free-brands-svg-icons";
+import { faLink } from "@fortawesome/free-solid-svg-icons";
+import { useToasts } from "react-toast-notifications";
 import { Wrapper, Container, ButtonGroup } from "./ShareSocialStyle";
 
 interface Props {
   url: string;
 }
 
-const ShareSocial: React.FC<Props> = ({ url }) => (
-  <Wrapper>
-    <Container>
-      <h5>Udostępnij</h5>
-      <ButtonGroup>
-        <a href="">
-          <Button background="white">tweet</Button>
-        </a>
-        <a href="">
-          <Button background="white">fb</Button>
-        </a>
-        <Button
-          background="white"
-          onClick={async () => {
-            await navigator.clipboard.writeText(url);
-          }}
-        >
-          copy
-        </Button>
-      </ButtonGroup>
-    </Container>
-    <Container>
-      <h5>Społeczność</h5>
-      <ButtonGroup>
-        <a href="">
-          <Button background="discord">Discord</Button>
-        </a>
-        <a href="">
-          <Button background="white">Sztab</Button>
-        </a>
-        <a href="">
-          <Button background="white">Polemika</Button>
-        </a>
-      </ButtonGroup>
-    </Container>
-  </Wrapper>
-);
+const ShareSocial: React.FC<Props> = ({ url }) => {
+  const { addToast } = useToasts();
+
+  return (
+    <Wrapper>
+      <Container>
+        <h5>Udostępnij</h5>
+        <ButtonGroup>
+          <a href="">
+            <Button background="white">
+              <FontAwesomeIcon icon={faTwitter} />
+            </Button>
+          </a>
+          <a href="">
+            <Button background="white">
+              <FontAwesomeIcon
+                icon={faFacebookF}
+                // The size of `faFacebookF` is 8px by default, making the button not perfectly squared.
+                style={{ width: "16px", height: "16px" }}
+              />
+            </Button>
+          </a>
+          <Button
+            background="white"
+            onClick={async () => {
+              await navigator.clipboard.writeText(url);
+              addToast("Copied URL to clipboard", { appearance: "success" });
+            }}
+          >
+            <FontAwesomeIcon icon={faLink} />
+          </Button>
+        </ButtonGroup>
+      </Container>
+      <Container>
+        <h5>Społeczność</h5>
+        <ButtonGroup>
+          <a href="https://discord.gg/k9MbvxapuM">
+            <Button
+              background="discord"
+              beforeIcon={<FontAwesomeIcon icon={faDiscord} />}
+            >
+              Discord
+            </Button>
+          </a>
+          <a href="facebook.com/groups/sztabmypolitics">
+            <Button
+              background="white"
+              beforeIcon={<FontAwesomeIcon icon={faFacebookF} />}
+            >
+              Sztab
+            </Button>
+          </a>
+          <a href="facebook.com/groups/polemika">
+            <Button
+              background="white"
+              beforeIcon={<FontAwesomeIcon icon={faFacebookF} />}
+            >
+              Polemika
+            </Button>
+          </a>
+        </ButtonGroup>
+      </Container>
+    </Wrapper>
+  );
+};
 
 export default ShareSocial;
