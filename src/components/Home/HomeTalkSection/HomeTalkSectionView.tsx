808d54a086e2fcfd0803ab8be5fca1847974cfea
Adrian Orłów <adrian@orlow.me>
2021-03-30 18:52:20 +0200

    Optimization

--- src/components/Home/HomeTalkSection/HomeTalkSectionView.tsx
+++ src/components/Home/HomeTalkSection/HomeTalkSectionView.tsx
@@ -4,14 +4,16 @@ import DefaultLink from "next/link";
 import { Section } from "@components/Home";
 import Button from "@shared/Button";
 import { BasicTalkPartsFragment } from "@generated/graphql";
-import { Link } from "@components/Talk";
 import useTranslation from "next-translate/useTranslation";
 import { paths } from "@constants";
+import dynamic from "next/dynamic";
 import { AdditionalContentWrapper } from "./HomeTalkSectionStyle";
 
 interface Props {
   talks: BasicTalkPartsFragment[];
 }
+
+const Link = dynamic(() => import("@components/Talk/TalkLink"));
 
 const HomeTalkSection: React.FC<Props> = ({ talks }) => {
   const { t } = useTranslation("home");
