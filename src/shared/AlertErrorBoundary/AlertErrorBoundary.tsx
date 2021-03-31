5c7661d57e636455f0c8a3adb3f999421e617b15
Piotr Romanowski <wsrh456@gmail.com>
2021-03-31 10:52:33 +0200

    Wrap disqus component in error boundary

---
+++ src/shared/AlertErrorBoundary/AlertErrorBoundary.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { ErrorBoundary } from "react-error-boundary";
+import Alert from "@shared/Alert";
+
+const AlertErrorBoundary: React.FC = ({ children }) => {
+  return (
+    <ErrorBoundary
+      fallbackRender={(e) => <Alert type="error">{e.error.message}</Alert>}
+    >
+      {children}
+    </ErrorBoundary>
+  );
+};
+
+export default AlertErrorBoundary;
