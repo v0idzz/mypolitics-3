e2e90e46763ea8dddd7d7740a5d720e3b036995b
Piotr Romanowski <wsrh456@gmail.com>
2021-05-02 17:04:43 +0200

    Add alt texts

--- src/components/Talk/TalkCategorySection/TalkCategorySectionView.tsx
+++ src/components/Talk/TalkCategorySection/TalkCategorySectionView.tsx
@@ -11,18 +11,22 @@ const assets = {
   [Enum_Talk_Type.Mvsp]: {
     logoAsset: require("@assets/images/talk/mvsp.png"),
     background: "linear-gradient(74.82deg, #8E2DE2 0%, #4A00E0 100%)",
+    alt: "Młodzież vs Politycy",
   },
   [Enum_Talk_Type.Ring]: {
     logoAsset: require("@assets/images/talk/ring.png"),
     background: "linear-gradient(91.81deg, #E2D02D 0%, #E00051 96.93%)",
+    alt: "Ring Polityczny",
   },
   [Enum_Talk_Type.Expert]: {
     logoAsset: require("@assets/images/talk/expert.png"),
     background: "linear-gradient(254.81deg, #2DE2D7 0%, #7D00E0 100.01%)",
+    alt: "Okiem Eksperta",
   },
   [Enum_Talk_Type.Classic]: {
     logoAsset: require("@assets/images/talk/classic.png"),
     background: `url(${require("@assets/images/talk/classic_bg.png")})`,
+    alt: "Debata Młodzieżówek",
   },
 };
 
@@ -32,12 +36,12 @@ interface Props {
 }
 
 const TalkCategorySection: React.FC<Props> = ({ talks, type }) => {
-  const { logoAsset, background } = assets[type];
+  const { logoAsset, background, alt } = assets[type];
 
   return (
     <Container className="container">
       <Header background={background}>
-        <Image src={logoAsset} alt="" />
+        <Image src={logoAsset} alt={alt} />
       </Header>
       <BaseSectionContent>
         <BaseSectionBackground />
